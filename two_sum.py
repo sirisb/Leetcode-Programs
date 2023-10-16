03839c07567639722dbde2c254bc7d0765a003aa
Siri Shivakumar <siri.shivakumar@gmail.com>
2023-10-16 11:23:13 -0700

    solve by using dictionary

--- two_sum.py
+++ two_sum.py
@@ -12,10 +12,26 @@ def two_sum(nums, target):
                     return i, j
 
 
+def two_sum1(nums, target):
+    num_map = {}
+    # put all nums to map.
+    for i in range (0, len(nums)):
+        num_map[nums[i]] = i
+    # find the 2nd num required by evaluating target - 1st num.
+    for i in range(0, len(nums)):
+        req = target - nums[i]
+        if num_map.get(req):
+            return i, num_map.get(req)
+
+    return None
+
+
 def main():
     nums = [2, 3, 1, 5, 20, 17]
-    print(two_sum(nums, 19))
+    print(two_sum(nums, 45))
     print(*nums, sep="->")
+
+    print(two_sum1(nums, 45))
 
 
 if __name__ == "__main__":
